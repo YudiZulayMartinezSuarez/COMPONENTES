f0a546e32e806c7fda0afaa2247f3b232ee6034e
havertzz04 <havertzz04@gmail.com>
2023-05-03 13:27:36 -0500

    feat: :fire: componentes final

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 import "./componentes/my-tabla.js"
 import "./componentes/my-section.js"
+import "./componentes/my-nav.js"
+import "./componentes/my-selecion.js"
+import "./componentes/my-footer.js"
---
+++ componentes/my-selecion.js
@@ -0,0 +1,19 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split("/").pop().replace(".js","");
+
+export default class mySelecion extends HTMLElement{
+    static async componentes(){
+        return await (await fetch(pathName.replace(".js",".html"))).text();
+    }
+
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"});
+        Promise.resolve(mySelecion.componentes()).then(html=>{
+            this.shadowRoot.innerHTML = html;
+        })
+        console.log("etiqueta rendirizada y configurada :)");
+    }
+}
+
+customElements.define(name, mySelecion);
